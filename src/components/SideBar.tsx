e4d35b0e969eed9093ebd798ccecad67ee92e202
Mufida A <49670663+mufidaandi@users.noreply.github.com>
2024-12-16 23:56:40 +0800

    create sidebar component

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,51 @@
+const SideBar = () => {
+  return (
+    <aside className=" text-white fixed w-80 h-screen top-0 left-0 bg-gradient-to-tr from-dark-brown to-brown-secondary">
+      <div className="p-4">
+      <img src="/écrire logo white.png" alt="ecrire logo" className="mb-4 w-4/12 m-auto" />
+        <ul>
+          <li className="mb-2 text-m">
+            <a href="#" className="flex items-center p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 mr-3"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+              Dashboard
+            </a>
+          </li>
+          {/* <li className="mb-2">
+            <a href="#" className="flex items-center p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 mr-3"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+              Profile
+            </a>
+          </li> */}
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default SideBar;
