0615a153f872f20fd576cd074ae2f89deea976f7
Mufida A <49670663+mufidaandi@users.noreply.github.com>
2024-12-21 22:09:09 +0800

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import UserAuth from './pages/UserAuth'
+import UserAuth from './pages/UserAuth'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Main from './pages/Main'
 
 function App() {
 
   return (
-    <>
-      {/* <UserAuth /> */}
-      <Main />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} /> 
+        <Route path="/auth" element={<UserAuth />} /> 
+      </Routes>
+    </Router>
   )
 }
 
